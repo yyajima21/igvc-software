ca26659b5e8874020b7fc9a72aea51384bafd401
rmkeezer <rmkeezer@yahoo.com>
2018-04-04 20:38:45 -0400

    add needed imports

--- igvc/src/tensorrt/main.cpp
+++ igvc/src/tensorrt/main.cpp
@@ -13,26 +13,21 @@
 #include <tf/transform_datatypes.h>
 #include <algorithm>
 #include <mutex>
-#include ""
+#include <cv_bridge/cv_bridge.h>
+#include <image_geometry/pinhole_camera_model.h>
+#include <image_transport/image_transport.h>
+#include <pcl/point_types.h>
+#include <pcl_ros/point_cloud.h>
+#include <tf/transform_listener.h>
 
-ros::Publisher disp_path_pub;
 
-void img_callback(const sensor_msgs::ImageConstPtr& msg, const sensor_msgs::CameraInfoConstPtr& cam_info)
+#include "trtengine.cpp"
+
+image_transport::Publisher pub;
+
+void img_callback(const cv::Mat msg_img, const sensor_msgs::ImageConstPtr& origMsg)
 {
-  cv_bridge::CvImagePtr cv_copy;
-  cv_copy = cv_bridge::toCvCopy(msg, "");
-  cv::Mat img = cv_copy->image;
-
-  img = ResizeCameraImage(img, 640, 360);
-  sensor_msgs::CameraInfo cam_info_rsz = ResizeCameraInfo(cam_info, 640, 360);
-
-  cam.fromCameraInfo(cam_info_rsz);
-  img_callback(img, msg);
-}
-
-void LineDetector::img_callback(const cv::Mat msg_img, const sensor_msgs::ImageConstPtr& origMsg)
-{
-  src_img = msg_img;
+  int i = 1;
 }
 
 int main(int argc, char** argv)
@@ -43,9 +38,11 @@ int main(int argc, char** argv)
 
   ros::NodeHandle pNh("~");
 
-  ros::Subscriber map_sub = nh.subscribe("/usb_cam_center/image_raw", 1, map_callback);
+  image_transport::ImageTransport _it(nh);
 
-  act_path_pub = nh.advertise<igvc_msgs::action_path>("/semantic_segmentation", 1);
+  image_transport::Subscriber map_sub = _it.subscribe("/usb_cam_center/image_raw", 1, img_callback);
+
+  pub = _it.advertise("/semantic_segmentation", 1);
 
   ros::spin();
 
