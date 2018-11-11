75e396947903329aebda4a8f2a493790902882f9
Andrew Tuttle <atuttle7@gatech.edu>
2018-11-11 16:37:27 -0500

    CNN todo notes

--- igvc_perception/src/cnn/cnn.py
+++ igvc_perception/src/cnn/cnn.py
@@ -16,18 +16,18 @@ from image_geometry import PinholeCameraModel
 
 class CNN:
 
-    def __init__(self, subscriber_topic, publisher_topic, model_filename, resize_width,
-            resize_height, force_cpu):
+    def __init__(self, subscriber_topic, publisher_topic, model_filename, resize_width, resize_height, force_cpu):
         self.graph = self.load_graph(model_filename)
         config = tf.ConfigProto(
-                        device_count = {'GPU': 1}
+            device_count={'GPU': 1}
         )
-        #if force_cpu:
+        # TODO:Diagnose why force cpu fails
+        # if force_cpu:
         #    self.sess = tf.Session(config=config,graph=self.graph)
-        #else:
+        # else:
+        # TODO: What is allow_growth?
         config.gpu_options.allow_growth = True
         self.sess = tf.Session(config=config,graph=self.graph)
-
 
         self.resize_width = resize_width
         self.resize_height = resize_height
@@ -35,7 +35,7 @@ class CNN:
 
         try:
             camera_info = rospy.wait_for_message('/usb_cam_center/camera_info', CameraInfo, timeout=5)
-        except(rospy.ROSException), e:
+        except rospy.ROSException, e:
             print "Camera info topic not available"
             print e
             exit()
@@ -65,7 +65,7 @@ class CNN:
         transform_listener = ros_tf.TransformListener()
         transform_listener.waitForTransform('/base_footprint', '/optical_cam_center', rospy.Time(0), rospy.Duration(5.0))
         cam_transform_translation, cam_transform_rotation = transform_listener.lookupTransform('/base_footprint', '/optical_cam_center', rospy.Time(0))
-        self.cam_transform_rotation_matrix = ros_tf.transformations.quaternion_matrix(cam_transform_rotation)[:-1,:-1]
+        self.cam_transform_rotation_matrix = ros_tf.transformations.quaternion_matrix(cam_transform_rotation)[:-1, :-1]
         self.cam_transform_translation = np.asarray(cam_transform_translation)
         print self.cam_transform_translation
         print self.cam_transform_rotation_matrix
@@ -76,7 +76,7 @@ class CNN:
         self.cloud_publisher = rospy.Publisher("/semantic_segmentation_cloud", PointCloud2, queue_size=1)
         self.subscriber = rospy.Subscriber(subscriber_topic, Image, self.image_callback, queue_size=1, buff_size=10**8)
 
-
+    # TODO: Explain this mechanism
     def load_graph(self, frozen_graph_filename):
         # We load the protobuf file from the disk and parse it to retrieve the
         # unserialized graph_def
@@ -91,18 +91,18 @@ class CNN:
             tf.import_graph_def(graph_def, name='')
         return graph
 
-
+    # TODO: Explain mechanism behind projection
     def init_point_cloud_array(self):
-        world_points_array = np.empty( (self.resize_height,self.resize_width,3), dtype=np.float32)
-        for r,c in np.ndindex(self.resize_height,self.resize_width):
-            ray = np.asarray( self.camera_model.projectPixelTo3dRay( (c,r) ) )
-            ray_in_world = np.matmul( self.cam_transform_rotation_matrix, ray )
+        world_points_array = np.empty((self.resize_height, self.resize_width, 3), dtype=np.float32)
+        for r, c in np.ndindex(self.resize_height, self.resize_width):
+            ray = np.asarray(self.camera_model.projectPixelTo3dRay((c, r)))
+            ray_in_world = np.matmul(self.cam_transform_rotation_matrix, ray)
             scale = -self.cam_transform_translation[2] / ray_in_world[2]
             world_point = scale * ray_in_world + self.cam_transform_translation
-            world_points_array[r,c] = [world_point[0], world_point[1], world_point[2]]
+            world_points_array[r, c] = [world_point[0], world_point[1], world_point[2]]
         self.world_point_array = world_points_array
 
-
+    # TODO: General method documentation
     def image_callback(self, image_msg):
         start = timer()
         try:
@@ -120,6 +120,7 @@ class CNN:
         shape = im.shape
         output_image = output[0][:, 1].reshape(shape[0], shape[1])
 
+        # TODO: Tune and parameterize
         threshold = 0.5
         im_threshold = output_image > threshold
 
@@ -127,11 +128,11 @@ class CNN:
 
         im_threshold = np.uint8(255*im_threshold)
         cv_output = cv2.cvtColor(im_threshold, cv2.COLOR_GRAY2BGR)
+        # TODO: Why not grayscale?
         msg_out = self.bridge.cv2_to_imgmsg(cv_output, 'bgr8')
         msg_out.header.stamp = image_msg.header.stamp
         self.im_publisher.publish(msg_out)
 
-        points = world_points
         cloud_msg = PointCloud2()
         cloud_msg.header.stamp = image_msg.header.stamp
         cloud_msg.header.frame_id = 'base_footprint'
@@ -151,16 +152,16 @@ class CNN:
         print end - start
 
 
-
 if __name__ == '__main__':
     rospy.init_node('cnn')
     image_topic = rospy.get_param('/image_topic')
+    # TODO: Check for image resize script
     image_resize_width = rospy.get_param('/image_resize_width')
     image_resize_height = rospy.get_param('/image_resize_height')
     model_path = rospy.get_param('/model_path')
     force_cpu = rospy.get_param('/force_cpu')
     CNN(image_topic, '/semantic_segmentation', model_path, image_resize_width,
-            image_resize_height, force_cpu)
+        image_resize_height, force_cpu)
 
     try:
         rospy.spin()
